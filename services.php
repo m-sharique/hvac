337c669f99a23ed37cb3aac2455ad6de01711f53
M Sharique Siddiqui <157228213+m-sharique@users.noreply.github.com>
2024-08-24 01:06:39 +0530

    Added a Little CSS and Changes a Few p tags to li tags

--- services.php
+++ services.php
@@ -8,8 +8,6 @@
   <meta name="keywords" content="HVAC, air-conditioning,ventilation,cooling,heating,maintenance,repairing,air quality,breeze,HVAC services, HVAC repair, air conditioning installation, furnace maintenance HVAC services in mumbai,air conditioning repair in mumbai, air conditioning repair in thane, furnace installation near me, HVAC repair services ,best HVAC contractor for residential cooling systems">
   <link rel="stylesheet" href="eb.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-  <link rel="stylesheet" href="eb.css">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
@@ -19,6 +17,11 @@
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
   <title>EliteBreeze|Services</title>
+  <style>
+    .card-body{
+      text-align: justify;
+    }
+  </style>
 </head>
 
 <body>
@@ -64,9 +67,9 @@
       <div class="card">
         <div class="card-body">
           <h3>HVAC Services in Mumbai</h3>
-          <p>&diams; Mumbai’s dynamic climate demands HVAC solutions that are both robust and efficient.</p>
-          <p>&diams; EliteBreeze offers a comprehensive range of services that cater to the unique environmental conditions of the city.</p>
-          <p>&diams; Our expert team is dedicated to keeping your HVAC systems running smoothly, ensuring your space remains comfortable and conducive to your lifestyle or business needs.</p>
+          <li> Mumbai’s dynamic climate demands HVAC solutions that are both robust and efficient.</li>
+          <li> EliteBreeze offers a comprehensive range of services that cater to the unique environmental conditions of the city.</li>
+          <li> Our expert team is dedicated to keeping your HVAC systems running smoothly, ensuring your space remains comfortable and conducive to your lifestyle or business needs.</li>
         </div>
       </div>
     </div>
@@ -75,9 +78,9 @@
       <div class="card">
         <div class="card-body">
           <h3>Comprehensive HVAC Maintenance</h3>
-          <p>&diams; Regular maintenance is essential to ensure the longevity and efficiency of your HVAC system.</p>
-          <p>&diams; At EliteBreeze, we offer thorough maintenance services designed to prevent issues before they arise, saving you time and money in the long run.</p>
-          <p>&diams; Our skilled technicians will inspect, clean, and fine-tune your system to ensure it operates at peak performance throughout the year.</p>
+          <li> Regular maintenance is essential to ensure the longevity and efficiency of your HVAC system.</li>
+          <li> At EliteBreeze, we offer thorough maintenance services designed to prevent issues before they arise, saving you time and money in the long run.</li>
+          <li> Our skilled technicians will inspect, clean, and fine-tune your system to ensure it operates at peak performance throughout the year.</li>
         </div>
       </div>
     </div>
@@ -86,8 +89,8 @@
       <div class="card">
         <div class="card-body">
           <h3>AMC for Commercial and Residential Properties</h3>
-          <p>&diams; Our Annual Maintenance Contracts (AMC) are designed to provide peace of mind to both residential and commercial clients.</p>
-          <p>&diams; With an EliteBreeze AMC, you can rest assured that your HVAC system will receive regular check-ups, maintenance, and priority service, reducing the risk of unexpected breakdowns and ensuring consistent comfort.</p>
+          <li> Our Annual Maintenance Contracts (AMC) are designed to provide peace of mind to both residential and commercial clients.</li>
+          <li> With an EliteBreeze AMC, you can rest assured that your HVAC system will receive regular check-ups, maintenance, and priority service, reducing the risk of unexpected breakdowns and ensuring consistent comfort.</li>
         </div>
       </div>
     </div>
@@ -96,8 +99,8 @@
       <div class="card">
         <div class="card-body">
           <h3>Air Conditioning Services</h3>
-          <p>In Mumbai's sweltering heat, a well-functioning air conditioning system is crucial.</p>
-          <p>EliteBreeze offers a full suite of air conditioning services to keep you cool and comfortable.</p>
+          <li>In Mumbai's sweltering heat, a well-functioning air conditioning system is crucial.</li>
+          <li>EliteBreeze offers a full suite of air conditioning services to keep you cool and comfortable.</li>
         </div>
       </div>
     </div>
@@ -106,8 +109,8 @@
       <div class="card">
         <div class="card-body">
           <h3>AC Repair</h3>
-          <p>&diams; Fast and reliable repair services to restore comfort quickly and effectively.</p>
-          <p>&diams; Whether it's a minor issue or a major malfunction, our technicians are equipped to handle all types of AC repairs.</p>
+          <li> Fast and reliable repair services to restore comfort quickly and effectively.</li>
+          <li> Whether it's a minor issue or a major malfunction, our technicians are equipped to handle all types of AC repairs.</li>
         </div>
       </div>
     </div>
@@ -116,9 +119,9 @@
       <div class="card">
         <div class="card-body">
           <h3>AC Maintenance</h3>
-          <p>&diams; Regular maintenance is key to keeping your air conditioning system running efficiently.</p>
-          <p>&diams; Our comprehensive AC maintenance services include filter cleaning, refrigerant checks, and system performance assessments to ensure your AC is always in top shape.</p>
-          <p>&diams; AC Installation: Need a new AC unit? Our team provides expert installation services tailored to your space, ensuring maximum efficiency and longevity of your new system.</p>
+          <li> Regular maintenance is key to keeping your air conditioning system running efficiently.</li>
+          <li> Our comprehensive AC maintenance services include filter cleaning, refrigerant checks, and system performance assessments to ensure your AC is always in top shape.</li>
+          <li> AC Installation: Need a new AC unit? Our team provides expert installation services tailored to your space, ensuring maximum efficiency and longevity of your new system.</li>
         </div>
       </div>
     </div>
@@ -127,9 +130,9 @@
       <div class="card">
         <div class="card-body">
           <h3>Duct Cleaning Services</h3>
-          <p>&diams; Clean ducts are essential for maintaining good indoor air quality and ensuring the efficiency of your HVAC system.</p>
-          <p>&diams; Over time, dust, debris, and allergens can accumulate in your ducts, reducing air quality and system performance.</p>
-          <p>&diams; EliteBreeze offers professional duct cleaning services to remove these contaminants, improving the air you breathe and the efficiency of your system.</p>
+          <li> Clean ducts are essential for maintaining good indoor air quality and ensuring the efficiency of your HVAC system.</li>
+          <li> Over time, dust, debris, and allergens can accumulate in your ducts, reducing air quality and system performance.</li>
+          <li> EliteBreeze offers professional duct cleaning services to remove these contaminants, improving the air you breathe and the efficiency of your system.</li>
         </div>
       </div>
     </div>
