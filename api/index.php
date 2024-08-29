f9f4107bc9315ca48fef4046b296523e41520e53
M Sharique Siddiqui <157228213+m-sharique@users.noreply.github.com>
2024-08-29 21:22:38 +0530

    Added a image using url

--- api/index.php
+++ api/index.php
@@ -165,6 +165,9 @@
         echo '</div>';
       }
       ?>
+      <div class="carousel-item active">
+        <img src="https://images.unsplash.com/photo-1545280405-f06710f1779d?q=80&w=1471&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="d-block w-100" alt="...">
+      </div>
     </div>
     <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
       <span class="carousel-control-prev-icon" aria-hidden="true"></span>
